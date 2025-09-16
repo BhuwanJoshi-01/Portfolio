114557ecd2f0a9b7541bfd80678cd71e552cedca
BhuwanJoshi-01 <bhuwanj779@gmail.com>
2025-09-16 22:04:55 +0545

    fixed white backgroud issue

--- main.jsx
+++ main.jsx
@@ -1,15 +1,10 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Example from './src/App.heroProfilePreview'
-import ErrorBoundary from '@kombai/react-error-boundary'
 import './src/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ErrorBoundary>
-      <KombaiWrapper>
         <Example />
-      </KombaiWrapper>
-    </ErrorBoundary>
   </StrictMode>,
 )
